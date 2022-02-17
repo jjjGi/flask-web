57c9a96a18d4f3a5bd1ca0e7ece9cc18ac821891
jjjGi <jiw720@naver.com>
2022-02-17 17:27:20 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request, jsonify
+from pymongo import MongoClient
+client = MongoClient('mongodb+srv://test:sparta@cluster0.87wks.mongodb.net/Cluster0?retryWrites=true&w=majority')
+db = client.dbsparta
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route("/homework", methods=["POST"])
+def homework_post():
+    nickname_receive = request.form['nickname_give']
+    comment_receive = request.form['comment_give']
+
+    doc = {'nickname': nickname_receive, 'comment': comment_receive}
+    db.guest_book.insert_one(doc)
+    return jsonify({'msg':'응원 완료!'})
+
+@app.route("/homework", methods=["GET"])
+def homework_get():
+    guest_list = list(db.guest_book.find({}, {'_id': False}))
+    return jsonify({'guests':guest_list})
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
